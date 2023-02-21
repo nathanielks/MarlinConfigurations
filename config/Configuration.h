2309c6dafb62d5e05ef074d03c25f345e5b9706d
Nathaniel <314009+nathanielks@users.noreply.github.com>
2023-02-21 14:06:27 -0500

    adjust nozzle probe offset

--- config/Configuration.h
+++ config/Configuration.h
@@ -1324,7 +1324,7 @@
  *     O-- FRONT --+
  */
 #define NOZZLE_TO_PROBE_OFFSET                                                 \
-  { 24, -37, -4.4 }
+  { 24, -37, -4.7 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
